e6046587577115b7004fb5e92f831aef0831c581
Luiz <luiz.guilhermesoutto@gmail.com>
2024-06-28 20:24:37 -0300

    validation friend added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 let amigos = [];
 
+        // Adiciona algum amigo
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+        // Validação para informar o nome do amigo
     if (amigo.value == '') {
         alert('Informe o nome do amigo!');
     }
+
+        // Verifica se o nome do amigo já está na lista de amigos
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+        // Organizando a linta de amigos e atualizando os participantes
     let lista = document.getElementById('lista-amigos');
 
     amigos.push(amigo.value);
@@ -18,13 +27,14 @@ function adicionar() {
     amigo.value = '';
 }
 
-
+        // Função para sortear os participantes (amigos)
 function sortear() {
+        // Aqui faz a validação para quantidade de participantes (Pelo menos 4 participantes)
     if (amigos.length < 4) {
         alert('Adicione pelo menos 4 participantes!');
         return;
     }
-
+        // Aqui faz o sorteio dos participantes (amigos)
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
@@ -37,18 +47,20 @@ function sortear() {
     }
 }
 
+        // Função para embaralhar os participantes (amigos) no final
 function embaralha(lista) {
 
     for (let indice = lista.length; indice; indice--) {
 
         const indiceAleatorio = Math.floor(Math.random() * indice);
 
-        // atribuição via destructuring
+        // Atribuição via String
         [lista[indice - 1], lista[indiceAleatorio]] = 
             [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
 
+        // Função para reiniciar a lista de amigos e a lista de sorteio
 function reiniciar() {
     amigos = [];
     document.getElementById('lista-amigos').innerHTML = '';
